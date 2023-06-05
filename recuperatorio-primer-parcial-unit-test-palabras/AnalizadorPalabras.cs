ea1cc3bf6b6af18c799509605fcdce722b241fe5
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2023-06-05 14:15:17 -0300

    Initial commit

---
+++ recuperatorio-primer-parcial-unit-test-palabras/AnalizadorPalabras.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recuperatorio_primer_parcial_unit_test_palabras
+{
+    public class AnalizadorPalabras
+    {
+        public bool EsPalidromo(String palabra)
+        {
+            if (!this.EsUnaSolaPalabra(palabra))
+            {
+                throw new ArgumentException("Solo se admite una palabra");
+            }
+            string reversedText = RevertirPalabra(palabra);
+            return palabra.Equals(reversedText);
+        }
+        private bool EsUnaSolaPalabra(string text)
+        {
+            if (text.Contains(' '))
+            {
+                return false;
+            }
+            return true;
+        }
+        private string RevertirPalabra(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ recuperatorio-primer-parcial-unit-test-palabras/Program.cs
@@ -0,0 +1,6 @@
+﻿using recuperatorio_primer_parcial_unit_test_palabras;
+
+AnalizadorPalabras analizadorPalabras = new AnalizadorPalabras();
+Console.WriteLine(analizadorPalabras.EsPalidromo("elefante"));
+Console.WriteLine(analizadorPalabras.EsPalidromo("neuquen"));
+Console.WriteLine(analizadorPalabras.EsPalidromo("santa fe"));
